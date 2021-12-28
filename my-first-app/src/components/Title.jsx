e42aa506ca9c0788d72c679c0091be8f163908bc
Jingyu Xiang <jxiang2@ualberta.ca>
2021-12-27 20:26:47 -0700

    done css

--- my-first-app/src/components/Title.jsx
+++ my-first-app/src/components/Title.jsx
@@ -4,7 +4,6 @@ export default function Title({title, subtitle}) {
     return (
         <div className='title-block'>
             <h1 className="title">{title}</h1>
-            <br/>
             <h2 className="subtitle">{subtitle}</h2>
         </div>
     )
