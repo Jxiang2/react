11bff6292f126bb5816eddd3c28aba1a223cb8a9
Jingyu Xiang <jxiang2@ualberta.ca>
2021-12-28 22:59:59 -0700

    add custom hooks

--- fetchdata-useeffect/src/components/TripList.jsx
+++ fetchdata-useeffect/src/components/TripList.jsx
@@ -1,19 +1,25 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import './TripList.css'
 
 export default function TripList() {
     const [trips, setTrips] = useState([])
     const [url, setUrl] = useState('http://127.0.0.1:3000/trips')
 
-    // use effect function will only run at the first evaluation
-    // orif any of it's dependencies change value
-    useEffect(()=>{
-        fetch(url)
-        .then(response => {return response.json()})
-        .then(json => setTrips(json))
+    // In JS, functions are changed after every re-evaluation
+    // solution: useCallback to cache the function, unless it's REALLY changed
+    const fetchTrips = useCallback( async() => {
+        const res = await fetch(url)
+        const json = await res.json()
+        setTrips(json)
     }, [url])
 
-  console.log(trips);
+    // use effect function will only run at the first evaluation
+    // or if ANY of it's dependencies change value
+    useEffect(()=>{
+        fetchTrips()
+    }, [fetchTrips])
+
+    // console.log(trips);
 
     return (
         <div className='trip-list'>
@@ -26,6 +32,7 @@ export default function TripList() {
                         <p>{trip.price}</p>
                     </li>))}
             </ul>
+
             <div className='filters'>
                 <button onClick={()=>setUrl("http://127.0.0.1:3000/trips?loc=europe")}>
                     European Trips
