d51dddf75ddd56a844c7e29609c586ad83f72fc8
Jingyu Xiang <jxiang2@ualberta.ca>
2021-12-30 00:00:39 -0700

    pages setup

---
+++ multipage-site/src/pages/About.jsx
@@ -0,0 +1,12 @@
+export default function About() {
+    return (
+        <div>
+            <h2>About</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Modi aspernatur ad culpa eius dolorem doloribus sequi fugit
+                voluptas illum maiores doloremque veniam necessitatibus dolorum 
+                similique architecto consequuntur, qui aut error?
+            </p>
+        </div>
+    )
+}
---
+++ multipage-site/src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+export default function Contact() {
+    return (
+        <div>
+            <h2>Contact</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Modi aspernatur ad culpa eius dolorem doloribus sequi fugit
+                voluptas illum maiores doloremque veniam necessitatibus dolorum 
+                similique architecto consequuntur, qui aut error?
+            </p>
+        </div>
+    )
+}
