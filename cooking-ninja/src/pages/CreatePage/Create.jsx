24ec2d3e8a5541f95878912e33e7a78888303a2d
Jingyu Xiang <jxiang2@ualberta.ca>
2021-12-31 15:32:23 -0700

    form done '

--- cooking-ninja/src/pages/CreatePage/Create.jsx
+++ cooking-ninja/src/pages/CreatePage/Create.jsx
@@ -1,9 +1,49 @@
+import { useState } from 'react'
 import './Create.css'
 
-export default function create() {
+export default function Create() {
+
+    const [title, setTitile] = useState('')
+    const [method, setMethod] = useState('')
+    const [cookingTime, setCookingTime] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(title, method, cookingTime);
+    }
+ 
     return (
-        <div>
-            Create
+        <div className='create'>
+            <h2 className='page-title'>Add a New Recipe</h2>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <span>Recipe title</span>
+                    <input type="text" 
+                     onChange={(e)=>setTitile(e.target.value)}
+                     value = {title}
+                     required/>
+                </label>
+
+                <label>
+                    <span>Recipe method</span>
+                    <textarea
+                     onChange={(e)=>setMethod(e.target.value)}
+                     value = {method}
+                     required/>
+                </label>
+
+                {/*ingredients go here*/}
+
+                <label>
+                    <span>Cooking time in minutes</span>
+                    <input type="number"
+                     onChange={(e)=>setCookingTime(e.target.value)}
+                     value = {cookingTime}
+                     required/>
+                </label>
+
+                <button className='btn'>Submit</button>
+            </form>
         </div>
     )
 }
--- cooking-ninja/src/pages/RecipiePage/Recipie.jsx
+++ cooking-ninja/src/pages/RecipiePage/Recipie.jsx
@@ -1,9 +1,27 @@
+import { useParams } from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch'
 import './Recipie.css'
 
 export default function Recipie() {
+
+    const {id} = useParams()
+    const url = `http://127.0.0.1:3000/recipes/${id}`
+    const {error, isPending, data: recipe} = useFetch(url)
+
     return (
-        <div>
-            Recipie
+        <div className='recipe'>
+            {error && <p className='error'>{error}</p>}
+            {isPending && <p className='loading'>Loading...</p>}
+            {recipe && (
+                <>
+                    <h2 className='page-title'>{recipe.title}</h2>
+                    <p>Tasks {recipe.cookingTime} to cook</p>
+                    <ul>
+                        {recipe.ingredients.map(ing=>(<li key={ing}>{ing}</li>))}
+                    </ul>
+                    <p className='method'>{recipe.method}</p>
+                </>
+            )}
         </div>
     )
 }
