470bed0dc9f5c3eb0969eb6e39a4c5a087ea6399
Jingyu Xiang <jxiang2@ualberta.ca>
2022-01-02 13:45:39 -0700

    update routers

--- my-money/src/App.jsx
+++ my-money/src/App.jsx
@@ -1,6 +1,19 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
+// pages
+import Home from './pages/HomePage/Home';
+import Login from './pages/LoginPage/Login';
+import Signup from './pages/SignupPage/Signup';
+
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Switch><Route exact path='/'><Home/></Route></Switch>
+        <Switch><Route exact path='/signup'><Signup/></Route></Switch>
+        <Switch><Route exact path='/login'><Login/></Route></Switch>
+      </BrowserRouter>
+
     </div>
   );
 }
