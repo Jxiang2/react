5b6aa601fb13c1d18d7373c128e7694ed44c1710
Jingyu Xiang <jxiang2@ualberta.ca>
2022-01-05 13:35:25 -0700

    real time transaction list done

--- my-money/src/hooks/useCollection.js
+++ my-money/src/hooks/useCollection.js
@@ -1,20 +1,26 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { projectFirestore } from "../config/config"
 
-export const useCollection = (collection) => {
+export const useCollection = (collection, _query) => {
 
     const [documents, setDocuments] = useState(null)
     const [error, setError] = useState(null)
 
+    // if don't use a ref --> infinite loop in useEffect
+    // _query is am reference ary and is diffrerent in memeory on every function call
+    const query = useRef(_query).current // current ary value
+
     useEffect(()=>{
         let ref = projectFirestore.collection(collection)
+        if (query) {
+            ref = ref.where(...query)
+        }
         const unsub = ref.onSnapshot((snapshot)=> {
             let resource = []
             // ary of documents
             snapshot.docs.forEach(doc => {
                 resource.push({...doc.data(), id:doc.id})
             })
-
             // update states
             setDocuments(resource)
             setError(null)
@@ -22,10 +28,9 @@ export const useCollection = (collection) => {
             console.log(error)
             setError(error.message)
         })
-
         // clearup function: unsubscribe on unmount
         return () => unsub()
-    }, [collection])
+    }, [collection, query])
 
     return {documents, error}
 }
