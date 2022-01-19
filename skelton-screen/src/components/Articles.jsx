4b7642d6aaff7a20deda1763a67e20ffac59426a
Jingyu Xiang <jxiang2@ualberta.ca>
2022-01-19 13:49:22 -0700

    setup data

--- skelton-screen/src/components/Articles.jsx
+++ skelton-screen/src/components/Articles.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 export default function Articles() {
+
+  const [articles, setArticles] = useState(null)
+  const currentArticles = useRef(articles).current
+
+  useEffect(()=>{
+    setTimeout(async()=>{
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+      const data = await res.json()
+      setArticles(data)
+    }, 1500)
+  }, [currentArticles])
+
+
   return (
     <div className='articles'>
         <h2>Articles</h2>
+
+        {articles && articles.map(article => (
+          <div className='article' key={article.id}>
+            <h3>{article.title}</h3>
+            <p>{article.body}</p>
+          </div>
+        ))}
+
+        {!articles && <div>loading...</div>}
     </div>
   )
 }
--- skelton-screen/src/components/User.jsx
+++ skelton-screen/src/components/User.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 export default function User() {
+
+  const [profile, setProfile] = useState(null)
+  const currentProfile = useRef(profile).current
+
+  useEffect(()=>{
+    setTimeout(async()=>{
+      const res = await fetch('https://jsonplaceholder.typicode.com/users/1')
+      const data = await res.json()
+      setProfile(data)
+    }, 1500)
+  }, [currentProfile])
+
   return (
     <div className='user'>
         <h2>User Profile</h2>
+        {profile && (
+          <div className='profile'>
+            <h3>{profile.username}</h3>
+            <p>{profile.email}</p>
+            <a href={profile.website}>{profile.website}</a>
+          </div>
+        )}
+
+        {!profile && <div>loading...</div>}
     </div>
   )
 }
