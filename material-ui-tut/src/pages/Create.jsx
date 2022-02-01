f3ac04880596db80aa6a157de9eed0355d1788bf
Jingyu Xiang <jxiang2@ualberta.ca>
2022-01-31 23:34:46 -0700

    mui content is outdated

--- material-ui-tut/src/pages/Create.jsx
+++ material-ui-tut/src/pages/Create.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
-import Typography from '@mui/material/Typography'
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight'
+import { Typography } from '@mui/material'
+import { Button } from '@mui/material'
+import { Container } from '@mui/material'
+
 
 export default function Create() {
+
   return (
-    <div>
-      <Typography variant='h1'>
+    <Container>
+      <Typography
+        variant='h6'
+        component='h2'
+        color='textSecondary'
+      >
         create new note
       </Typography>
-    </div>
+
+      <Button
+      type='submit'
+      color='secondary'
+      variant='contained'
+      endIcon={<KeyboardArrowRightIcon/>}
+      onClick={()=>console.log('you clicked me')}
+      >
+        Submit
+      </Button>
+    </Container>
   )
 }
