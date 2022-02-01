ae8658bfd7ee12bba1817d73b15eb81885987cdb
Jingyu Xiang <jxiang2@ualberta.ca>
2022-01-31 22:35:24 -0700

    download dependencies

--- material-ui-tut/src/pages/Create.jsx
+++ material-ui-tut/src/pages/Create.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Typography from '@mui/material/Typography'
 
 export default function Create() {
   return (
     <div>
-      Create page
+      <Typography variant='h1'>
+        create new note
+      </Typography>
     </div>
   )
 }
