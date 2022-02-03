5f57c1777d80ec2c299c44976a64cdc188e7ad34
Jingyu Xiang <jxiang2@ualberta.ca>
2022-02-03 00:05:30 -0700

    preparation done

--- travel-advisor/src/App.js
+++ travel-advisor/src/App.js
@@ -1,7 +1,27 @@
-import "./App.css";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+import PlaceDetails from "./components/PlaceDetail/PlaceDetails";
+
+import { CssBaseline, Grid } from "@material-ui/core";
 
 function App() {
-	return <div className='App'>hello world</div>;
+	return (
+		<>
+			<CssBaseline />
+			<Header />
+
+			<Grid container spacing={3} style={{ width: "100%" }}>
+				<Grid item xs={12} md={4}>
+					<List />
+				</Grid>
+
+				<Grid item xs={12} md={8}>
+					<Map />
+				</Grid>
+			</Grid>
+		</>
+	);
 }
 
 export default App;
