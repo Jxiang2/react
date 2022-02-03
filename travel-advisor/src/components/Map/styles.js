987302233dec772aea048a0001afac672854aba9
Jingyu Xiang <jxiang2@ualberta.ca>
2022-02-03 11:32:58 -0700

    restyle app

--- travel-advisor/src/components/Map/styles.js
+++ travel-advisor/src/components/Map/styles.js
@@ -9,8 +9,13 @@ export default makeStyles(() => ({
 		width: "100px",
 	},
 	mapContainer: {
+		marginTop: "15px",
 		height: "85vh",
 		width: "100%",
+		border: "1px solid lightgrey",
+		boxShadow: "3px 3px 5px rgba(0,0,0,0.05)",
+		background: "lightgrey",
+		borderRadius: "1px",
 	},
 	markerContainer: {
 		position: "absolute",
