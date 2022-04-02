6d9183e07a5740ac8a6f9233f3e08ea548d4e973
Jingyu Xiang <jxiang2@ualberta.ca>
2022-04-02 00:11:04 -0600

    downgrade to react 17

--- playground/src/pages/UseRef.tsx
+++ playground/src/pages/UseRef.tsx
@@ -1,7 +1,18 @@
+import { useState, useRef, useEffect } from "react";
+
 export default function UseRef () {
+
+    const [name, setName] = useState("");
+    const rerenderCount = useRef(1);
+
+    useEffect(() => {
+        rerenderCount.current = rerenderCount.current + 1;
+    });
+
     return (
         <div>
-            useRef
-        </div>
-    );
+            <input value={ name } onChange={ e => setName(e.target.value) } />
+            <div>My name is { name }</div>
+            <div>I rendered { rerenderCount.current }</div>
+        </div>);
 }
