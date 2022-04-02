3907e3709ccbbcaadbc5b873879e947adebd721a
Jingyu Xiang <jxiang2@ualberta.ca>
2022-04-02 16:22:01 -0600

    themeContext is setup

--- playground/src/contexts/Theme.ts
+++ playground/src/contexts/Theme.ts
@@ -1,7 +1,9 @@
 export interface Theme {
-  "backgroundColor": string;
+  backgroundColor: string;
 };
 
+export type ThemeActions = { type: "CHANGE_COLOR", payload: string; };
+
 export const initTheme: Theme = {
-  "backgroundColor": "#947673"
+  backgroundColor: "#947673"
 };
--- playground/src/contexts/ThemeContext.tsx
+++ playground/src/contexts/ThemeContext.tsx
@@ -1,16 +1,31 @@
-import React, { createContext } from "react";
-import { Theme, initTheme } from "./Theme";
+import React, { createContext, useReducer } from "react";
+import { Theme, ThemeActions, initTheme } from "./Theme";
 
 type ThemeContextProviderProps = {
   children: React.ReactNode;
 };
 
-export const ThemeContex = createContext<Theme>(initTheme);
+const themeReducer = (state: Theme, action: ThemeActions) => {
+  switch (action.type) {
+    case "CHANGE_COLOR":
+      return { ...state, backgroundColor: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const ThemeContex = createContext(initTheme);
 
 export const ThemeContexProvider = ({ children }: ThemeContextProviderProps) => {
+  // logics
+  const [state, dispatch] = useReducer(themeReducer, initTheme);
+  const changeTheme = (color: string) => {
+    dispatch({ type: "CHANGE_COLOR", payload: color });
+  };
+
   return (
-    <ThemeContex.Provider value={ initTheme }>
+    <ThemeContex.Provider value={ state } >
       { children }
-    </ThemeContex.Provider>
+    </ThemeContex.Provider >
   );
 };
