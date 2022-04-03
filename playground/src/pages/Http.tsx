251903c1b6adaed0ed8643532328a346d53cd2e8
Jingyu Xiang <jxiang2@ualberta.ca>
2022-04-03 11:58:44 -0600

    axios methods done

--- playground/src/pages/Http.tsx
+++ playground/src/pages/Http.tsx
@@ -1,12 +1,30 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { themeContext } from "../contexts/themeContext";
 import { useAxios } from "../Hooks/useAxios";
+import { useAxiosGet } from "../Hooks/useAxiosGet";
 
 export default function UseEffect () {
     const theme = useContext(themeContext);
+    const [url, setUrl] = useState("http://3.144.236.185:8000/planets");
+    const { response } = useAxiosGet(url, "");
     useAxios();
 
+    const changeUrl = () => {
+        if (url === "http://3.144.236.185:8000/planets")
+            setUrl("http://3.144.236.185:8000/launches");
+        else
+            setUrl("http://3.144.236.185:8000/planets");
+    };
+
+    useEffect(() => {
+        if (response) console.log(response);
+    }, [response]);
+
     return (
-        <div style={ { backgroundColor: theme?.backgroundColor } }>HTTP CRUD Workflow</div>
+        <div style={ { backgroundColor: theme?.backgroundColor } }>
+            HTTP CRUD Workflow
+            <p>This Page is for demostrating the useAxios and useAxiosGet hook</p>
+            <button onClick={ changeUrl }>change url</button>
+        </div>
     );
 }
