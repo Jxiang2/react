f07ac09ba2bd11b373a21c116f830cceb165df89
Jingyu Xiang <jxiang2@ualberta.ca>
2022-04-04 11:26:31 -0600

    submit form done

--- playground/src/index.tsx
+++ playground/src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
-import App from "./App";
 
+import App from "./App";
 import { ThemeContexProvider } from "./contexts/themeContext";
 
 import "./index.css";
