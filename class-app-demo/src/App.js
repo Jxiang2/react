24c2dcae6d8a4577fdf908543f9d1ff94aeb3228
Henry Xiang <henryxiang@Henrys-MacBook-Pro.local>
2022-06-01 23:26:32 -0400

    play with class lifecycles

--- class-app-demo/src/App.js
+++ class-app-demo/src/App.js
@@ -1,10 +1,37 @@
-import './App.css'
+import React, { Component } from 'react'
+import Counter from "./Counter"
 
-function App () {
-  return (
-    <div className="App">
-    </div>
-  )
+export default class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      mount: true,
+      propToIgnore: 0,
+      num: 0
+    }
+
+    this.adder = () => this.setState({ num: this.state.num + 1 })
+    this.mountCounter = () => this.setState({ mount: true })
+    this.unmountCounter = () => this.setState({ mount: false })
+    this.propToIgnore = () => this.setState({ propToIgnore: Math.random() })
+  }
+
+  render () {
+
+    return (
+      <div>
+        <button onClick={this.adder}>{this.state.num}</button>
+
+        <button disabled={this.state.mount} onClick={this.mountCounter}>mountCounter</button>
+
+        <button disabled={!this.state.mount} onClick={this.unmountCounter}>unmountCounter</button>
+
+        <button style={{ marginBottom: "20px" }} onClick={this.propToIgnore}>Change propToIgnore</button>
+
+        {this.state.mount ? <Counter propToIgnore={this.state.propToIgnore} /> : null}
+      </div>
+    )
+
+  }
 }
-
-export default App
---
+++ class-app-demo/src/Counter.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+
+export default class Counter extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      counter: 0
+    }
+
+    this.increment = () => this.setState({ counter: this.state.counter + 1 })
+    this.decrement = () => this.setState({ counter: this.state.counter - 1 })
+  }
+
+  // invoked right after render
+  componentDidMount () {
+    console.log("Component did mount----------------")
+  }
+
+  shouldComponentUpdate (nextProps, nextState) {
+    if (nextProps.propToIgnore && this.props.propToIgnore !== nextProps.propToIgnore) {
+      console.log("Should component update - no render----------------")
+      return false
+    }
+
+    console.log("Should component update - render----------------")
+    return true
+  }
+
+  render () {
+    console.log("render----------------")
+
+    return (
+      < div className="App" >
+        Counter: {this.state.counter}
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
+        {this.props.propToIgnore}
+      </ div>
+    )
+  }
+
+  componentDidUpdate (prevProps, prevState, snapshot) {
+    console.log("Component did update, the prev state is", prevState, "----------------")
+  }
+
+  componentWillUnmount () {
+    console.log("Component will unmount----------------")
+  }
+}
