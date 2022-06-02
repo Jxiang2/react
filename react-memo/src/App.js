e23ce1143b0adad219713396ec4aa857996d3102
Henry Xiang <henryxiang@Henrys-MacBook-Pro.local>
2022-06-01 21:33:18 -0400

    useMemo is nice when passing ref datatypes!

---
+++ react-memo/src/App.js
@@ -0,0 +1,41 @@
+import { useState, memo, useMemo } from "react"
+import './App.css'
+
+function Swatch ({ params }) {
+  console.log(`Swatch  rendered ${params.color}`)
+
+  return (
+    <div style={{ margin: 2, width: 75, height: 75, backgroundColor: params.color }}></div>
+  )
+}
+
+// maintain the content of Swatch by memo it, compare previous props to new props
+// when props change, the memoized component re-renders
+const MemoedSwatch = memo(Swatch)
+
+function App () {
+  const [appRenderIndex, setAppRenderIndex] = useState(0)
+  const [color, setColor] = useState("red")
+  const params = useMemo(() => ({ color }), [color]) // create the {color}, only changes when color changes
+
+  console.log(`App rendered ${appRenderIndex}`)
+
+  return (
+    <div className="App">
+      <button onClick={() => setAppRenderIndex(prev => appRenderIndex + 1)}>
+        re-render app
+      </button>
+
+      <button onClick={() => setColor(color === "red" ? "blue" : "red")}>
+        change color
+      </button>
+
+      <MemoedSwatch params={params} />
+
+      {/* another independent instance of MemoedSwatch */}
+      {/* <MemoedSwatch params={{ color: color === "red" ? "blue" : "red" }} /> */}
+    </div>
+  )
+}
+
+export default App
