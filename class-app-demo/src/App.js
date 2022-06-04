dd41834f60a88bb4491ab346952407e9441b981f
Jingyu Xiang <jxiang2@ualberta.ca>
2022-06-04 13:11:18 -0400

    test git

--- class-app-demo/src/App.js
+++ class-app-demo/src/App.js
@@ -1,37 +1,40 @@
-import React, { Component } from 'react'
-import Counter from "./Counter"
+import React, { Component } from 'react';
+import Counter from "./Counter";
 
 export default class App extends Component {
   constructor (props) {
-    super(props)
+    super(props);
 
     this.state = {
       mount: true,
       propToIgnore: 0,
       num: 0
-    }
+    };
 
-    this.adder = () => this.setState({ num: this.state.num + 1 })
-    this.mountCounter = () => this.setState({ mount: true })
-    this.unmountCounter = () => this.setState({ mount: false })
-    this.propToIgnore = () => this.setState({ propToIgnore: Math.random() })
+    this.adder = () => this.setState({ num: this.state.num + 1 });
+
+    this.mountCounter = () => this.setState({ mount: true });
+
+    this.unmountCounter = () => this.setState({ mount: false });
+
+    this.propToIgnore = () => this.setState({ propToIgnore: Math.random() });
   }
 
   render () {
 
     return (
       <div>
-        <button onClick={this.adder}>{this.state.num}</button>
+        <button onClick={ this.adder }>{ this.state.num }</button>
 
-        <button disabled={this.state.mount} onClick={this.mountCounter}>mountCounter</button>
+        <button disabled={ this.state.mount } onClick={ this.mountCounter }>mountCounter</button>
 
-        <button disabled={!this.state.mount} onClick={this.unmountCounter}>unmountCounter</button>
+        <button disabled={ !this.state.mount } onClick={ this.unmountCounter }>unmountCounter</button>
 
-        <button style={{ marginBottom: "20px" }} onClick={this.propToIgnore}>Change propToIgnore</button>
+        <button style={ { marginBottom: "20px" } } onClick={ this.propToIgnore }>Change propToIgnore</button>
 
-        {this.state.mount ? <Counter propToIgnore={this.state.propToIgnore} /> : null}
+        { this.state.mount ? <Counter propToIgnore={ this.state.propToIgnore } /> : null }
       </div>
-    )
+    );
 
   }
 }
