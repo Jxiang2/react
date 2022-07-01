eced308503012d18b8c486c12b5fcd9885f7020c
Jxiang2 <xjyhenry@gmail.com>
2022-07-01 13:24:25 -0400

    webpack react example

---
+++ create-react-app/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <div>hello</div>
+
+        <button>click me</button>
+      </>
+    )
+  }
+}
---
+++ create-react-app/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const HTMLWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+
+  module: {
+    rules: [
+      // bable rule
+      {
+        test: /.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react',]
+          },
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HTMLWebPackPlugin({
+      template: './src/index.html' // inject bundle.js into index.html in src dir
+    }),
+  ],
+}
