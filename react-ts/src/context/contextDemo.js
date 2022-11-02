962f2e850b8a0d2b91b2b8c6ed511e2a175f016e
Jxiang2 <xjyhenry@gmail.com>
2022-11-01 22:15:30 -0400

    test async action with handler pattern, works!

--- react-ts/src/context/contextDemo.js
+++ react-ts/src/context/contextDemo.js
@@ -1,38 +1,38 @@
-import React from 'react'
+import React from "react";
 
 // original approach
-export const UserContext1 = React.createContext()
+export const UserContext1 = React.createContext();
 
 export default function App() {
   return (
     <UserContext1.Provider value="Reed">
       <User />
     </UserContext1.Provider>
-  )
+  );
 }
 
 function User() {
   return (
     <UserContext1.Consumer>
-      {value => <h1>{value}</h1>}
+      {(value) => <h1>{value}</h1>}
       {/* prints: Reed */}
     </UserContext1.Consumer>
-  )
+  );
 }
 
 // with hook
-export const UserContext2 = React.createContext()
+// export const UserContext2 = React.createContext()
 
-export default function App() {
-  return (
-    <UserContext2.Provider value="Reed">
-      <User />
-    </UserContext2.Provider>
-  )
-}
+// export default function App() {
+//   return (
+//     <UserContext2.Provider value="Reed">
+//       <User />
+//     </UserContext2.Provider>
+//   )
+// }
 
-function User() {
-  const value = React.useContext(UserContext2)
+// function User() {
+//   const value = React.useContext(UserContext2)
 
-  return <h1>{value}</h1>
-}
+//   return <h1>{value}</h1>
+// }
