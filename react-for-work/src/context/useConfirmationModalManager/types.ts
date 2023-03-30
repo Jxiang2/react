6421e3511ede21ddfd3259612268046a6d0ef8a1
Jxiang2 <xjyhenry@gmail.com>
2023-03-30 00:04:31 -0400

    add

--- react-for-work/src/context/useConfirmationModalManager/types.ts
+++ react-for-work/src/context/useConfirmationModalManager/types.ts
@@ -1,22 +1,23 @@
 import { OPEN_CONFIRMATION_MODAL, CLOSE_CONFIRMATION_MODAL } from "./constants";
 
 export type ModalCallback = (confirmed: boolean) => void;
-export type ConfirmationModalPayload = {
+export interface ConfirmationModalPayload {
   title: string;
   content: string;
   handleConfirm: ModalCallback;
-};
-type ConfirmationModalHandlers = {
+}
+interface ConfirmationModalHandlers {
   handlers: {
     openModal: (payload: ConfirmationModalPayload) => void;
     closeModal: () => void;
   };
-};
-export type ConfirmationModalState = {
+}
+export interface ConfirmationModalState
+  extends ConfirmationModalPayload,
+    ConfirmationModalHandlers {
   isOpen: boolean;
-} & ConfirmationModalPayload &
-  ConfirmationModalHandlers;
-export type ConfirmationModalEvents = {
+}
+export interface ConfirmationModalEvents {
   [OPEN_CONFIRMATION_MODAL]: ConfirmationModalPayload;
   [CLOSE_CONFIRMATION_MODAL]: null;
-};
+}
