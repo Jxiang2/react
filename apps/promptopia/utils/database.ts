97c5e2b33d2753aa22500cdac1562dbbcef19cd6
Jxiang2 <xjyhenry@gmail.com>
2023-05-20 23:56:28 -0400

    add

--- apps/promptopia/utils/database.ts
+++ apps/promptopia/utils/database.ts
@@ -1,26 +1,27 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 let isConnected = false; // track the connection
 
 export const connectToDB = async () => {
-  mongoose.set('strictQuery', true);
+  mongoose.set("strictQuery", true);
 
-  if(isConnected) {
-    console.log('MongoDB is already connected');
+  if (isConnected) {
+    console.log("MongoDB is already connected");
     return;
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGODB_URI as string, {
       dbName: "share_prompt",
+      // @ts-ignore
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    })
+    });
 
     isConnected = true;
 
-    console.log('MongoDB connected')
+    console.log("MongoDB connected");
   } catch (error) {
     console.log(error);
   }
-}
+};
