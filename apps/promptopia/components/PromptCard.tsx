8dc12db80a65e4419eb8d5d3a15704f1213cf40f
Jxiang2 <xjyhenry@gmail.com>
2023-06-09 22:08:05 -0400

    add

--- apps/promptopia/components/PromptCard.tsx
+++
@@ -1,3 +0,0 @@
-export function PromptCard() {
-  return <div>PromptCard</div>;
-}
--- apps/promptopia/components/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./Feed";
-export * from "./Nav";
-export * from "./Profile";
-export * from "./PromptCard";
-export * from "./Provider";
